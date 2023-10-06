2385dbc0fc923e8a7a9ae30bc886a13cfcfb1d8c
Lukas Spreitzer <l.spreitzer@limesoda.com>
2023-10-06 16:05:05 +0200

    Disable copilot for empty buffers

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -5,12 +5,15 @@ return {
   cmd = 'Copilot',
   event = 'InsertEnter',
   opts = {
+    copilot_node_command = '/Users/l.spreitzer/.local/share/nvm/v16.20.2/bin/node',
+    filetypes = {
+      [''] = false,
+    },
     panel = {
       enabled = false,
     },
     suggestion = {
       enabled = false,
     },
-    copilot_node_command = '/Users/l.spreitzer/.local/share/nvm/v16.20.2/bin/node'
   },
 }
