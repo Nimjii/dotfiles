2888a7a0652c15cede7c1ecd917000c452874c94
Nimjii <lspreitzer525@gmail.com>
2024-07-13 19:43:06 +0200

    Add html treesitter injections for alpine

---
+++ nvim/.config/nvim/queries/html/injections.scm
@@ -0,0 +1,46 @@
+; extends
+
+(
+  (attribute
+    (attribute_name) @attribute_name
+    (quoted_attribute_value
+      (attribute_value) @injection.content
+    )
+  )
+  (#match? @attribute_name "^(x-init|x-show|x-bind|x-text|x-html|x-model|x-modelable|x-for|x-effect|x-if|x-id|(x-on|\\@|:).*)$")
+  (#set! injection.language "javascript")
+)
+
+(
+  (attribute
+    (attribute_name) @attribute_name
+    (quoted_attribute_value
+      (attribute_value) @injection.content
+    )
+  )
+  (#eq? @attribute_name "x-data")
+  (#not-match? @injection.content "f:format")
+  (#set! injection.language "javascript")
+)
+
+(
+  (element
+    (self_closing_tag
+      (tag_name) @tag_name
+      (attribute
+	(quoted_attribute_value
+	  (attribute_value) @variable_name
+	)
+      )
+      (attribute
+	(quoted_attribute_value
+	  (attribute_value) @injection.content
+	)
+      )
+    )
+  )
+  (#eq? @tag_name "f:variable")
+  (#match? @variable_name "[xX][dD]ata")
+  (#set! injection.language "javascript")
+)
+
