00bc543eae8f29f229b1a5252b878368c31aedf5
Nimjii <lspreitzer525@gmail.com>
2025-06-30 13:52:47 +0200

    Customize copilot mappings

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,5 +1,13 @@
 -- @module autocmds.lua
 
+-- [[ Enable copilot suggestions when exiting insert mode ]]
+vim.api.nvim_create_autocmd('InsertLeave', {
+  pattern = '*',
+  callback = function ()
+    vim.b.copilot_suggestion_hidden = false
+  end,
+})
+
 -- [[ Handle trailing whitespaces and empty lines ]]
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = { '*' },
--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -5,18 +5,19 @@ return {
   cmd = 'Copilot',
   event = 'InsertEnter',
   opts = {
-    copilot_node_command = vim.fn.expand('$HOME') .. '/.local/share/nvm/v20.15.0/bin/node',
+    copilot_node_command = vim.fn.expand '$HOME' .. '/.local/share/nvm/v20.15.0/bin/node',
     filetypes = {
       [''] = false,
     },
     suggestion = {
       auto_trigger = true,
       hide_during_completion = false,
+      trigger_on_accept = false,
       keymap = {
-        accept = '<Tab>',
+        accept = nil,
         next = '<C-n>',
         prev = '<C-p>',
-        dismiss = '<C-e>',
+        dismiss = nil,
       },
     },
     server_opts_overrides = {
@@ -28,7 +29,51 @@ return {
     },
   },
   keys = {
-    { '<leader>cp', mode = 'n', function () require('copilot.panel').open({}) end, desc = 'Show panel' },
-    { '<leader>ct', mode = 'n', function () require('copilot.suggestion').toggle_auto_trigger() end, desc = 'Toggle auto trigger' },
+    {
+      '<Tab>',
+      mode = 'i',
+      function()
+        if require('copilot.suggestion').is_visible() then
+          require('copilot.suggestion').accept()
+        else
+          return '<Tab>'
+        end
+
+        return ''
+      end,
+      desc = 'Tab or accept copilot suggestion',
+      expr = true,
+    },
+    {
+      '<C-e>',
+      mode = 'i',
+      function()
+        if vim.b.copilot_suggestion_hidden then
+          require('copilot.suggestion').next()
+        else
+          require('copilot.suggestion').dismiss()
+        end
+
+        vim.b.copilot_suggestion_hidden = not vim.b.copilot_suggestion_hidden
+      end,
+      desc = 'Tab or accept copilot suggestion',
+      expr = true,
+    },
+    {
+      '<leader>cp',
+      mode = 'n',
+      function()
+        require('copilot.panel').open {}
+      end,
+      desc = 'Show panel',
+    },
+    {
+      '<leader>ct',
+      mode = 'n',
+      function()
+        require('copilot.suggestion').toggle_auto_trigger()
+      end,
+      desc = 'Toggle auto trigger',
+    },
   },
 }
