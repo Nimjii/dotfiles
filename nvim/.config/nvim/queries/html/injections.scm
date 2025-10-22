8a5cba610f9e4dd083a6073aeb396434db3d5401
Nimjii <lspreitzer525@gmail.com>
2025-10-22 13:17:10 +0200

    Adjust html javascript injection for alpine.js

--- nvim/.config/nvim/queries/html/injections.scm
+++ nvim/.config/nvim/queries/html/injections.scm
@@ -7,7 +7,8 @@
       (attribute_value) @injection.content
     )
   )
-  (#match? @attribute_name "^(x-init|x-show|x-bind|x-text|x-html|x-model|x-modelable|x-for|x-effect|x-if|x-id|x-trap|(x-on|\\@|:).*)$")
+  (#match? @attribute_name "^(x-init|x-show|x-bind|x-text|x-html|x-model|x-modelable|x-for|x-effect|x-ref|x-teleport|x-if|x-id|x-trap|(x-intersect|x-on|\\@|:).*)$")
+  (#not-match? @injection.content "f:")
   (#set! injection.language "javascript")
 )
 
@@ -19,7 +20,7 @@
     )
   )
   (#eq? @attribute_name "x-data")
-  (#not-match? @injection.content "f:format")
+  (#not-match? @injection.content "f:")
   (#set! injection.language "javascript")
 )
 
@@ -43,4 +44,3 @@
   (#match? @variable_name "[xX][dD]ata")
   (#set! injection.language "javascript")
 )
-
